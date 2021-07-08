f8eaec0f3731ec1cd189dd9b8a193fa07ef13eea
CansecoDev <git@canseco.dev>
2021-07-08 02:03:14 +0200

    Make the procedure hide Sticker Messages instead

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function hideBlocked(){
-    const blocked = document.querySelectorAll('[class^="groupStart"]'); // Find all "Blocked Messages"
-    blocked.forEach(blokMsg => {
-        if(blokMsg.style.display !== "none") blokMsg.style.display = "none"; // Hide the message if it's not already hidden.
+function hideStickerMessages(){
+    const stickerMessages = document.querySelectorAll('[class^="clickableSticker"]'); // Find all Messages with stickers on it
+    stickerMessages.forEach(stikMsg => {
+        if(stikMsg.style.display !== "none") stikMsg.style.display = "none"; // Hide the message if it's not already hidden.
     });
 };
-setInterval(hideBlocked, 500); // Repeat every half second. Recommended to keep at 500, but raise/ lower if you wish.
+setInterval(hideStickerMessages, 500); // Repeat every half second. Recommended to keep at 500, but raise/ lower if you wish.
